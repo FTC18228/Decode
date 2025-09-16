9a276d52b93cc17836136a0008808fc96cdc2ebb
FTC18228 <18228@buildingblockstudio.com>
2025-09-16 19:49:31 +1000

    Update test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LaunchTestOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LaunchTestOpmode.java
@@ -1,32 +1,49 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.commands.TurretShootCommand;
+import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
+
+import java.util.function.BooleanSupplier;
+
 @TeleOp(name="Launch Test", group="tests")
-public class LaunchTestOpmode extends LinearOpMode {
+public class LaunchTestOpmode extends CommandOpMode {
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        DcMotor leftTurret = hardwareMap.get(DcMotor.class, "turretLeft");
-        DcMotor rightTurret = hardwareMap.get(DcMotor.class, "turretRight");
+    public void initialize() {
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+        TurretSubsystem turretSubsystem = new TurretSubsystem(hardwareMap);
 
-        waitForStart();
+        new Trigger(new BooleanSupplier() {
+            @Override
+            public boolean getAsBoolean() {
+                return gamepad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.5;
+            }
+        }).whenActive(
+                new InstantCommand(turretSubsystem::fireTurret)
+        ).whenInactive(
+                new InstantCommand(turretSubsystem::stopTurret)
+        );
 
-        while(opModeIsActive()) {
-            if(gamepad1.left_trigger >= 0.5) {
-                leftTurret.setPower(-   1);
+        new Trigger(new BooleanSupplier() {
+            @Override
+            public boolean getAsBoolean() {
+                return gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.5;
             }
-            else {leftTurret.setPower(0);}
-            if(gamepad1.right_trigger >= 0.5) {
-                rightTurret.setPower(1);
-            }
-            else {rightTurret.setPower(0);}
-        }
+        }).whenActive(
+                new InstantCommand(turretSubsystem::fireTurret)
+        ).whenInactive(
+                new InstantCommand(turretSubsystem::stopTurret)
+        );
     }
 }
